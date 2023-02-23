bd99f2f17bc4cea9b3e6d2a6c854ccda4fa9fb4a
lokkochan <chunlok.chan@ucalgary.ca>
2023-02-22 19:48:37 -0700

    Start a little

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import {useState} from 'react';
+
 function App() {
-  return <h1>Lotion</h1>;
+  const [notes, setNotes] =useState([]) ;
+  const [note, setNote] = useState('');
+  const [showAllNotes, setShowAllNotes] = useState(false);
+  return (
+  <>
+    <header>
+      <button id="show_all_notes">-</button>
+      <div id="title">
+        <h1>Lotion</h1>
+        <h4>Like Notion, but worst</h4>
+      </div>
+    </header>
+    <div id="content">
+      <div id="notelist">
+      </div>
+      <div id="note">
+      </div>
+    </div>
+
+  </>);
 }
 
 export default App;
